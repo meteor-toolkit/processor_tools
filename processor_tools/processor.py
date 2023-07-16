c61aface4493223c8dfc7459be4b4ad95264a6b1
sam.hunt <sam.hunt@npl.co.uk>
2023-07-16 16:02:31 +0100

    initial definition BaseProcessor class

--- processor_tools/processor.py
+++ processor_tools/processor.py
@@ -1,5 +1,8 @@
 """processor_tools.processor - processor class definition"""
 
+from typing import Optional, Any
+
+
 __author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
 __all__ = ["BaseProcessor"]
 
@@ -7,9 +10,16 @@ __all__ = ["BaseProcessor"]
 class BaseProcessor:
     """
     Base class for processor implementations
+
+    :param context: container object storing configuration values that define the processor state
     """
 
-    pass
+    def __init__(self, context: Optional[Any] = None):
+        """
+        Constructor method
+        """
+
+        self.context = context if context is not None else {}
 
 
 if __name__ == "__main__":
--- processor_tools/tests/test_processor.py
+++ processor_tools/tests/test_processor.py
@@ -1,13 +1,22 @@
 """processor_tools.tests.test_processor - tests for processor_tools.test_processor"""
 
 import unittest
+from processor_tools.processor import BaseProcessor
 
 __author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
 __all__ = []
 
 
 class TestBaseProcessor(unittest.TestCase):
-    pass
+    def test___init___None(self):
+        p = BaseProcessor()
+
+        self.assertEqual(type(p.context), dict)
+
+    def test___init__(self):
+        p = BaseProcessor("a")
+
+        self.assertEqual(p.context, "a")
 
 
 if __name__ == "__main__":
