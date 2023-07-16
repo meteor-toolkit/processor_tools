8926d2905777dfebd5623d47e9362f1a8a968018
sam.hunt <sam.hunt@npl.co.uk>
2023-07-16 15:36:03 +0100

    initialise processor_builder modules and test module

---
+++ processor_tools/processor_builder.py
@@ -0,0 +1,7 @@
+"""processor_tools.processor_builder - tools for building configurable processors"""
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+__all__ = []
+
+if __name__ == "__main__":
+    pass
---
+++ processor_tools/tests/test_processor_builder.py
@@ -0,0 +1,9 @@
+"""processor_tools.tests.test_processor_builder - tests for processor_tools.test_processor_builder"""
+
+import unittest
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+__all__ = []
+
+if __name__ == "__main__":
+    pass
