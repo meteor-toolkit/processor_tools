d0e89d2a2ef88989bb064d85abf66df4d28d12bb
sam.hunt <sam.hunt@npl.co.uk>
2024-05-04 17:53:01 +0100

    init DatabaseCRUD class and tests

---
+++ processor_tools/db_crud.py
@@ -0,0 +1,16 @@
+"""processor_tools.db_crud - utilities for creating, reading, updating and deleting databases"""
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+__all__ = ["DatabaseCRUD"]
+
+
+class DatabaseCRUD:
+    """
+    Class containing utilities for creating, reading, updating and deleting databases.
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ processor_tools/tests/test_db_crud.py
@@ -0,0 +1,14 @@
+"""processor_tools.tests.test_db_crud - tests for processor_tools.db_crud"""
+
+import unittest
+
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+
+
+class TestDatabaseCRUD(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
