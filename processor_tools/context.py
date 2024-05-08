0fcfd6c53a211195048f39c76c738c88b8096545
sam.hunt <sam.hunt@npl.co.uk>
2024-05-08 14:42:25 +0100

    initialise Context class and test class

---
+++ processor_tools/context.py
@@ -0,0 +1,11 @@
+"""processor.context - customer container from processor state"""
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+
+
+class Context:
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ processor_tools/tests/test_context.py
@@ -0,0 +1,13 @@
+"""processor.tests.test_context - tests for processor_tools.context"""
+
+import unittest
+
+__author__ = "Sam Hunt <sam.hunt@npl.co.uk>"
+
+
+class TestContext(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
